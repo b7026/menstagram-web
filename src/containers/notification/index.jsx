6ed9952b99aa6acfda8cc9724d132d7be8972a9c
b7026 <44283710+b7026@users.noreply.github.com>
2019-11-26 18:07:26 +0900

    Update index.jsx

--- src/containers/notification/index.jsx
+++ src/containers/notification/index.jsx
@@ -39,26 +39,6 @@ export class Notification extends Component {
       </div>
       <div　className="text-center mb-4">通知はありません</div>
         </div>
-=======
-
-export class Notification extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-          <div className="position-relative py-3 border-bottom mb-3">
-           <div className="text-center mb-4">通知</div>
-           <div className="d-flex justify-content-around">
-            <button type="button">いいね</button>
-            <button type="button">フォロー</button>
-            <button type="button">運営</button>
-           </div>
-          </div>
-      </div>
->>>>>>> 70a55ee25630e0ec490f5e57e5af3047bfa05311
     );
   }
 }
